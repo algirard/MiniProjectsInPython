20358b4fccc558acdcdba6ecb65b1fddfae9d311
commitalex <31814565+commitalex@users.noreply.github.com>
2021-05-04 18:45:24 -0400

    Add files via upload
    Updated code to work with python3.

--- 3. Mad Libs Generator/src/MadLibsGenerator.py
+++ 3. Mad Libs Generator/src/MadLibsGenerator.py
@@ -2,20 +2,20 @@
 def story():
     template = "Didn't you hear? Supposedly %(noun)s %(verb)s is the new %(color)s."
     dictionary = {}
-    dictionary["noun"] = raw_input("Enter a noun: ")
-    dictionary["verb"] = raw_input("Enter a -ing verb: ")
-    dictionary["color"] = raw_input("Enter a color: ")
-    print template % dictionary
+    dictionary["noun"] = input("Enter a noun: ")
+    dictionary["verb"] = input("Enter a -ing verb: ")
+    dictionary["color"] = input("Enter a color: ")
+    print(template % dictionary)
 
 def story2():
     template = "Every %(period_of_time)s I like to %(verb)s and %(verb2)s my %(emotion)s %(noun)s."
     dictionary = {}
-    dictionary["period_of_time"] = raw_input("Enter a length of time: ").lower()
-    dictionary["verb"] = raw_input("Enter a verb: ").lower()
-    dictionary["verb2"] = raw_input("Enter another verb: ").lower()
-    dictionary["emotion"] = raw_input("Enter an emotion: ").lower()
-    dictionary["noun"] = raw_input("Enter a noun: ")
-    print template % dictionary
+    dictionary["period_of_time"] = input("Enter a length of time: ").lower()
+    dictionary["verb"] = input("Enter a verb: ").lower()
+    dictionary["verb2"] = input("Enter another verb: ").lower()
+    dictionary["emotion"] = input("Enter an emotion: ").lower()
+    dictionary["noun"] = input("Enter a noun: ")
+    print(template % dictionary)
     
 from random import choice
 stories = [story, story2]
